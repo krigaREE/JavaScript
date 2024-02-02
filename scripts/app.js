8fb783a28ece71e4495ff21cf6ffc15349a75057
krigaREE <krigarepkr@gmail.com>
2024-02-02 17:22:09 +0200

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+alert('This Works');
+
+let currentResult = 0;
+
+currentResult = currentResult + 10
+
+let calculationDescription =  '(0 + 10) * 3 / 2 - 1';
+let errorMessage = 'An error occured';
+
+outputResult(currentResult,'calculationDescription');
